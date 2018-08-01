27f6c15bb053787ee424598ca849458a70cb15b0
Gladiator <gladiatorlearns@gmail.com>
2018-08-01 19:40:25 +0530

    additional

--- 2dnumpy.py
+++ 2dnumpy.py
@@ -40,3 +40,19 @@ np_baseball=np.array(baseball)
 # Print out the shape of np_baseball
 print(np_baseball)
 print(np_baseball.shape)
+
+
+# baseball is available as a regular list of lists
+# updated is available as 2D numpy array
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (3 cols)
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+print(np_baseball+updated)
+
+# Create numpy array: conversion
+conversion=np.array([0.0254,0.453592,1])
